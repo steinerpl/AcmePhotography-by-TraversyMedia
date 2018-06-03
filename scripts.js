c3f6aa6cc10ba4825d770d4a6be131e80ad6ee32
Grzegorz Grochowski <steinerpl@gmail.com>
2018-06-03 19:42:58 +0200

    Added tooltip

--- scripts.js
+++ scripts.js
@@ -7,3 +7,52 @@ $('#view-work').on('click', function() {
 		scrollTop: images
 	}, 900);
 });
+
+(function() {
+
+	var tooltip = null;
+
+	function createToolTip(text, options) {
+		var div = document.createElement("div");
+		div.textContent = text;
+		div.className = "tooltip hidden";
+
+		document.body.appendChild(div);
+
+		div.style.left = (options.x + options.w/2 - div.offsetWidth/2 ) + "px";
+		div.style.top = (options.y - div.offsetHeight - 9) + "px";
+
+		div.classList.remove("hidden");
+
+		tooltip = div;
+	}
+
+	function showToolTip(e) {
+		
+		console.log(e);
+		var title = e.target.getAttribute("title");
+		e.target.removeAttribute("title");
+
+		createToolTip(title, {
+			w: e.target.offsetWidth,
+			x: e.target.offsetLeft,
+			y: e.target.offsetTop
+		});
+
+	}
+	function removeToolTip(e) {
+		
+		e.target.setAttribute("title", tooltip.textContent);
+		tooltip.parentNode.removeChild(tooltip);
+
+	}
+
+	var title = document.querySelectorAll("[title]");
+	
+	for( var i = 0; i < title.length; i++ ) {
+		title[i].addEventListener("mouseenter", showToolTip, false);
+		title[i].addEventListener("mouseleave", removeToolTip, false);
+	}
+
+
+})();
